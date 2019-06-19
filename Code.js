463dbf94a0d7ea4ac5d46d7002ec5c4fbe22b637
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2019-06-19 14:39:16 -0400

    First stab at refactoring this goddamn spaghetti code; I really need to find a way to add automated testing to this thing

--- Code.js
+++ Code.js
@@ -1,44 +1,56 @@
+// === DEFINITIONS ===
 var PPI = 72;
 
-function doGet() {
-  return ContentService.createTextOutput('I just successfully handled your GET request.');
+var PositionX = {
+  NOT_SET: 0,
+  LEFT: 1,
+  RIGHT: 2,
+  CENTER: 3
 }
 
+var PositionY = {
+  NOT_SET: 0,
+  TOP: 1,
+  BOTTOM: 2,
+  CENTER: 3
+}
+
+// === INIT ===
 function onOpen(event) {
   SlidesApp.getUi().createAddonMenu()
     .addSubMenu(SlidesApp.getUi().createMenu('Precision snap')
-      .addItem('To dimension', 'applyParanoiaToDimension')
-      .addItem('To position', 'applyParanoiaToPosition')
-      .addItem('To rotation', 'applyParanoiaToRotation')
-      .addItem('Straighten elements', 'applyZeroRotation'))
+      .addItem('To dimension', 'menuSnapDimension')
+      .addItem('To position', 'menuSnapPosition')
+      .addItem('To rotation', 'menuSnapRotation')
+      .addItem('Straighten elements', 'menuZeroRotation'))
     .addSubMenu(SlidesApp.getUi().createMenu('Copy attributes')
-      .addItem('Width', 'copyWidth')
-      .addItem('Height', 'copyHeight')
-      .addItem('Width and height', 'copyWidthAndHeight')
-      .addItem('Rotation', 'copyRotation'))
+      .addItem('Width', 'menuCopyWidth')
+      .addItem('Height', 'menyCopyHeight')
+      .addItem('Width and height', 'menuCopyWidthAndHeight')
+      .addItem('Rotation', 'menuCopyRotation'))
     .addSeparator()
     .addSubMenu(SlidesApp.getUi().createMenu('Adjoin')
-      .addItem('Horizontally', 'adjoinH')
-      .addItem('Vertically', 'adjoinV'))
+      .addItem('Horizontally', 'menuAdjoinH')
+      .addItem('Vertically', 'menuAdjoinV'))
     .addSubMenu(SlidesApp.getUi().createMenu('Align to inner edge')
-      .addItem('Left', 'alignToInnerLeft')
-      .addItem('Right', 'alignToInnerRight')
-      .addItem('Horizontal center', 'alignToHorizontalCenter')
+      .addItem('Left', 'menuAlignToInnerLeft')
+      .addItem('Right', 'menuAlignToInnerRight')
+      .addItem('Horizontal center', 'menuAlignToHorizontalCenter')
       .addSeparator()
-      .addItem('Top', 'alignToInnerTop')
-      .addItem('Bottom', 'alignToInnerBottom')
-      .addItem('Vertical center', 'alignToVerticalCenter'))
+      .addItem('Top', 'menuAlignToInnerTop')
+      .addItem('Bottom', 'menuAlignToInnerBottom')
+      .addItem('Vertical center', 'menuAlignToVerticalCenter'))
     .addSubMenu(SlidesApp.getUi().createMenu('Align to outer edge')
-      .addItem('Left', 'alignToOuterLeft')
-      .addItem('Right', 'alignToOuterRight')
+      .addItem('Left', 'menuAlignToOuterLeft')
+      .addItem('Right', 'menuAlignToOuterRight')
       .addSeparator()
-      .addItem('Top', 'alignToOuterTop')
-      .addItem('Bottom', 'alignToOuterBottom'))
-    .addItem('Center on page', 'centerOnPage')
+      .addItem('Top', 'menuAlignToOuterTop')
+      .addItem('Bottom', 'menuAlignToOuterBottom'))
+    .addItem('Center on page', 'menuCenterOnPage')
     .addSubMenu(SlidesApp.getUi().createMenu('Flip')
-      .addItem('Horizontally', 'flipH')
-      .addItem('Vertically', 'flipV')
-      .addItem('Both', 'flipHandV'))
+      .addItem('Horizontally', 'menuFlipH')
+      .addItem('Vertically', 'menuFlipV')
+      .addItem('Both', 'menuFlipHV'))
     .addSeparator()
     .addSubMenu(SlidesApp.getUi().createMenu('Set transparency')
       .addItem('100% (transparent)', 'menuSetTransparency0')
@@ -55,51 +67,75 @@ function onOpen(event) {
       .addItem('Invert background colors', 'menuSetColorInverse')
       .addItem('Max text contrast', 'menuSetColorMaxContrast'))
     .addSeparator()
-    .addItem('About', 'showAboutSidebar')
+    .addItem('About', 'menuShowAboutSidebar')
     .addToUi();
 }
 
-function tableDebug() {
-  var elements = getElementArray();
-  SlidesApp.getUi().alert(elements[0].asTable().getObjectId() + ";" + elements[0].asTable().getWidth());
-  elements[0].asTable().setColumnWidth(0, 200);
-}
+// === MENU CALLS ===
+function menuSnapPosition()  { getElementArray().forEach(function(e) { snapElement(e, true, false, false); }); }
+function menuSnapDimension() { getElementArray().forEach(function(e) { snapElement(e, false, true, false); }); }
+function menuSnapRotation()  { getElementArray().forEach(function(e) { snapElement(e, false, false, true); }); }
+function menuZeroRotation()  { getElementArray().forEach(function(e) { e.setRotation(0); }); }
 
-function showAboutSidebar() {
+function menuCopyWidth() { copySelectedElementDimensions(true, false, false); }
+function menuCopyHeight() { copySelectedElementDimensions(false, true, false); }
+function menuCopyWidthAndHeight() { copySelectedElementDimensions(true, true, false); } 
+function menuCopyRotation() { copySelectedElementDimensions(false, false, true); } 
+
+function menuAdjoinH() { adjoinSelectedElements(0*PPI, true, true); }
+function menuAdjoinV() { adjoinSelectedElements(0*PPI, false, true); }
+
+function menuAlignToInnerLeft()        { align(PositionX.LEFT, PositionY.NOT_SET, false); }
+function menuAlignToInnerRight()       { align(PositionX.RIGHT, PositionY.NOT_SET, false); }
+function menuAlignToInnerTop()         { align(PositionX.NOT_SET, PositionY.TOP, false); }
+function menuAlignToInnerBottom()      { align(PositionX.NOT_SET, PositionY.BOTTOM, false); }
+function menuAlignToOuterLeft()        { align(PositionX.LEFT, PositionY.NOT_SET, true); }
+function menuAlignToOuterRight()       { align(PositionX.RIGHT, PositionY.NOT_SET, true); }
+function menuAlignToOuterTop()         { align(PositionX.NOT_SET, PositionY.TOP, true); }
+function menuAlignToOuterBottom()      { align(PositionX.NOT_SET, PositionY.BOTTOM, true); }
+function menuAlignToVerticalCenter()   { align(PositionX.NOT_SET, PositionY.CENTER, false); }
+function menuAlignToHorizontalCenter() { align(PositionX.CENTER, PositionY.NOT_SET, false); }
+
+function menuFlipH() { flipSelectedElements(true, false); }
+function menuFlipV() { flipSelectedElements(false, true); }
+function menuFlipHV() { flipSelectedElements(true, true); }
+
+function menuSetTransparency0()   { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0 }); }
+function menuSetTransparency10()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.1 }); }
+function menuSetTransparency25()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.25 }); }
+function menuSetTransparency33()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.33 }); }
+function menuSetTransparency50()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.5 }); }
+function menuSetTransparency66()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.67 }); }
+function menuSetTransparency75()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.75 }); }
+function menuSetTransparency90()  { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.9 }); }
+function menuSetTransparency100() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 1 }); }
+
+function menuSetColorSwap() { doStuffWithSelectedOrAllShapesOnPage(doColorSwap); }
+function menuSetColorInverse() { doStuffWithSelectedOrAllShapesOnPage(doColorInversion); }
+function menuSetColorMaxContrast() { doStuffWithSelectedOrAllShapesOnPage(doMaxTextContrast); }
+
+function menuShowAboutSidebar() {
   var ui = HtmlService
     .createHtmlOutputFromFile('about')
     .setTitle('Slides Power Toys');
   SlidesApp.getUi().showSidebar(ui);
 }
 
-function roundTo(number, digits) {
-  var power = Math.pow(10, digits);
-  return Math.round(number * power) / power;
+function menuCenterOnPage() {
+  var elementArray = getElementArray();
+  
+  // In this case, reference is always the page
+  elementArray.push(fakePageAsReferenceElement());
+
+  var n = elementArray.length-1;
+  
+  for (var i = 0; i < n; i++) {
+    alignShape(elementArray[n], elementArray[i], PositionX.CENTER, PositionY.CENTER, false);
+  }
 }
 
-function getSelectionType() {
-  return SlidesApp.getActivePresentation().getSelection().getSelectionType();
-}
-
-function applyParanoiaToPosition() {
-  getElementArray().forEach(function(e) {
-    applyParanoiaToElement(e, true, false, false);
-  });
-}
-
-function applyParanoiaToDimension() {
-  getElementArray().forEach(function(e) {
-    applyParanoiaToElement(e, false, true, false);
-  });
-}
-
-function applyParanoiaToRotation() {
-  getElementArray().forEach(function(e) {
-    applyParanoiaToElement(e, false, false, true);
-  });
-}
-
-function applyParanoiaToElement(element, shouldApplyToPosition, shouldApplyToDimension, shouldApplyToRotation) {
+// === CORE LOGIC ===
+function snapElement(element, shouldApplyToPosition, shouldApplyToDimension, shouldApplyToRotation) {
   if (shouldApplyToPosition) {
     element.setLeft(roundTo(element.getLeft()/PPI, 1)*PPI);
     element.setTop(roundTo(element.getTop()/PPI, 1)*PPI);
@@ -113,20 +149,8 @@ function applyParanoiaToElement(element, shouldApplyToPosition, shouldApplyToDim
   if (shouldApplyToRotation)
     element.setRotation(roundTo(element.getRotation(), 0));
 }
-
-function flipH() {
-  flip(true, false);
-}
-
-function flipV() {
-  flip(false, true);
-}
-
-function flipHandV() {
-  flip(true, true);
-}
   
-function flip(shouldFlipH, shouldFlipV) {
+function flipSelectedElements(shouldFlipH, shouldFlipV) {
   var elementArray = getElementArray();
   
   if (elementArray.length < 2)
@@ -148,15 +172,7 @@ function flip(shouldFlipH, shouldFlipV) {
   }
 }   
 
-function adjoinH() {
-  adjoin(0*PPI, true, true);
-}
-
-function adjoinV() {
-  adjoin(0*PPI, false, true);  
-}
-
-function adjoin(paddingPoints, shouldAdjoinH, shouldCenterOnFirst) {
+function adjoinSelectedElements(paddingPoints, shouldAdjoinH, shouldCenterOnFirst) {
   var elementArray = getElementArray();
   
   // Can only adjoin multiple elements. Also, skip if no elements were selected, but maybe revist this later...
@@ -178,106 +194,14 @@ function adjoin(paddingPoints, shouldAdjoinH, shouldCenterOnFirst) {
   }
 }
 
-function applyZeroRotation() {
-  getElementArray().forEach(function(e) { e.setRotation(0); });
-}
-
-function getElementArray() {
-  var selection = SlidesApp.getActivePresentation().getSelection();
-  var elementArray = [];
-  
-  switch (selection.getSelectionType()) {
-    case (SlidesApp.SelectionType.CURRENT_PAGE):
-      // Page is selected, so fallback to all elements in page
-      elementArray = selection.getCurrentPage().getPageElements();
-      //Logger.log("Applying to all elements on page.");
-      break;
-    case (SlidesApp.SelectionType.TEXT):
-      // Text is selected, so fallback to parent container
-      elementArray = selection.getPageElementRange().getPageElements();
-      break;
-    case (SlidesApp.SelectionType.PAGE_ELEMENT || SlidesApp.SelectionType.TEXT):
-      // Elements are selected, so apply only to them
-      elementArray = selection.getPageElementRange().getPageElements();
-      break;
-    }
-
-    return elementArray;
-}
-
-function isPageSelected() {
-  return SlidesApp.getActivePresentation().getSelection().getSelectionType() == SlidesApp.SelectionType.CURRENT_PAGE;
-}
-
-function alignToInnerLeft() {
-  align(PositionX.LEFT, PositionY.NOT_SET, false);
-}
-
-function alignToInnerRight() {
-  align(PositionX.RIGHT, PositionY.NOT_SET, false);
-}
-
-function alignToInnerTop() {
-  align(PositionX.NOT_SET, PositionY.TOP, false);
-}
-
-function alignToInnerBottom() {
-  align(PositionX.NOT_SET, PositionY.BOTTOM, false);  
-}
-
-function alignToOuterLeft() {
-  align(PositionX.LEFT, PositionY.NOT_SET, true);
-}
-
-function alignToOuterRight() {
-  align(PositionX.RIGHT, PositionY.NOT_SET, true);
-}
-
-function alignToOuterTop() {
-  align(PositionX.NOT_SET, PositionY.TOP, true);
-}
-
-function alignToOuterBottom() {
-  align(PositionX.NOT_SET, PositionY.BOTTOM, true);  
-}
-
-function alignToVerticalCenter() {
-  align(PositionX.NOT_SET, PositionY.CENTER, false);
-}
-
-function alignToHorizontalCenter() {
-  align(PositionX.CENTER, PositionY.NOT_SET, false);
-}
-
-function getPageAsFakeElement() {
-  return { getLeft: function() { return 0; },
-          getTop: function() { return 0; },
-          getWidth: function() { return SlidesApp.getActivePresentation().getPageWidth(); },
-          getHeight: function() { return SlidesApp.getActivePresentation().getPageHeight(); }};
-}
-
-function centerOnPage() {
-  var elementArray = getElementArray();
-  
-  // In this case, reference is always the page
-  elementArray.push(getPageAsFakeElement());
-
-  var n = elementArray.length-1;
-  
-  for (var i = 0; i < n; i++) {
-    alignShape(elementArray[n], elementArray[i], PositionX.CENTER, PositionY.CENTER, false);
-  }
-}
-
+// This behavior is fundamentally different from the native alignment tools,
+//   because here order matters (instead of position of elements on page)
 function align(positionX, positionY, isOuterEdge) {
-  // This behavior is fundamentally different from the native alignment tools,
-  //   because here order matters (instead of position of elements on page)
-
   var elementArray = getElementArray();
 
   // If no elements were selected or only 1 element was selected, reference element becomes the page itself. Wish G Apps Script would support lambdas for a cleaner syntax...
   if (isPageSelected() || elementArray.length == 1)
-    elementArray.push(getPageAsFakeElement());
+    elementArray.push(fakePageAsReferenceElement());
 
   // Reference is always the last element. This is the only consistent way of selecting a reference element on G Apps since the selection array's order is not preserved
   var n = elementArray.length-1;
@@ -307,23 +231,7 @@ function alignShape(referenceShape, targetShape, positionX, positionY, isOuterEd
     targetShape.setTop(referenceShape.getTop() + referenceShape.getHeight()/2 - targetShape.getHeight()/2);
 }
 
-function copyWidth() {
-  copyDimensions(true, false, false);
-}
-
-function copyHeight() {
-  copyDimensions(false, true, false);
-}
-
-function copyWidthAndHeight() {
-  copyDimensions(true, true, false);
-} 
-
-function copyRotation() {
-  copyDimensions(false, false, true);
-} 
-
-function copyDimensions(copyWidth, copyHeight, copyRotation) {
+function copySelectedElementDimensions(copyWidth, copyHeight, copyRotation) {
   if (isPageSelected())
     return; // no selection array to get, so won't be able to get a reference element
 
@@ -340,37 +248,7 @@ function copyDimensions(copyWidth, copyHeight, copyRotation) {
   }
 }
 
-var PositionX = {
-  NOT_SET: 0,
-  LEFT: 1,
-  RIGHT: 2,
-  CENTER: 3
-}
-
-var PositionY = {
-  NOT_SET: 0,
-  TOP: 1,
-  BOTTOM: 2,
-  CENTER: 3
-}
-
-function menuSetTransparency0() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0 }); }
-function menuSetTransparency10() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.1 }); }
-function menuSetTransparency25() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.25 }); }
-function menuSetTransparency33() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.33 }); }
-function menuSetTransparency50() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.5 }); }
-function menuSetTransparency66() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.67 }); }
-function menuSetTransparency75() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.75 }); }
-function menuSetTransparency90() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 0.9 }); }
-function menuSetTransparency100() { doStuffWithSelectedOrAllShapesOnPage(doSetAlpha, { amount: 1 }); }
-
-function menuSetColorSwap() { doStuffWithSelectedOrAllShapesOnPage(doColorSwap); }
-function menuSetColorInverse() { doStuffWithSelectedOrAllShapesOnPage(doColorInversion); }
-function menuSetColorMaxContrast() { doStuffWithSelectedOrAllShapesOnPage(doMaxTextContrast); }
-
-function doStuffWithSelectedOrAllShapesOnPage(doStuff) {
-  doStuffWithSelectedOrAllShapesOnPage(doStuff, {});
-}
+function doStuffWithSelectedOrAllShapesOnPage(doStuff) { doStuffWithSelectedOrAllShapesOnPage(doStuff, {}); }
 function doStuffWithSelectedOrAllShapesOnPage(doStuff, attributes) {
   var elementArray = getElementArray();  
   var n = elementArray.length;
@@ -424,16 +302,49 @@ var doMaxTextContrast = function(shape, attributes) {
   }
 }
 
-function doesShapeHaveSolidFill(shape) {
-  return shape.getFill().getType() == SlidesApp.FillType.SOLID;
-}
-function doesShapeHaveText(shape) {
-  // Using getLength() > 1 because isEmpty() is broken; it's probably counting the new line character
-  //   that documentation (https://developers.google.com/apps-script/reference/slides/shape#gettext) 
-  //   says always exists, which is a non-sensical behavior. I can see this breaking some day.
-  return shape.getText().getLength() > 1;
+// === SHORTCUT FUNCTIONS ===
+function getSelectionType() { return SlidesApp.getActivePresentation().getSelection().getSelectionType(); }
+
+function isPageSelected() { return SlidesApp.getActivePresentation().getSelection().getSelectionType() == SlidesApp.SelectionType.CURRENT_PAGE; }
+function doesShapeHaveSolidFill(shape) { return shape.getFill().getType() == SlidesApp.FillType.SOLID; }
+
+// Using getLength() > 1 because isEmpty() is broken; it's probably counting the new line character
+//   that documentation (https://developers.google.com/apps-script/reference/slides/shape#gettext) 
+//   says always exists, which is a non-sensical behavior. I can see this breaking some day.
+function doesShapeHaveText(shape) { return shape.getText().getLength() > 1; }
+
+
+function getElementArray() {
+  var selection = SlidesApp.getActivePresentation().getSelection();
+  var elementArray = [];
+  
+  switch (selection.getSelectionType()) {
+    case (SlidesApp.SelectionType.CURRENT_PAGE):
+      // Page is selected, so fallback to all elements in page
+      elementArray = selection.getCurrentPage().getPageElements();
+      //Logger.log("Applying to all elements on page.");
+      break;
+    case (SlidesApp.SelectionType.TEXT):
+      // Text is selected, so fallback to parent container
+      elementArray = selection.getPageElementRange().getPageElements();
+      break;
+    case (SlidesApp.SelectionType.PAGE_ELEMENT || SlidesApp.SelectionType.TEXT):
+      // Elements are selected, so apply only to them
+      elementArray = selection.getPageElementRange().getPageElements();
+      break;
+    }
+
+    return elementArray;
 }
 
+function fakePageAsReferenceElement() {
+  return { getLeft: function() { return 0; },
+          getTop: function() { return 0; },
+          getWidth: function() { return SlidesApp.getActivePresentation().getPageWidth(); },
+          getHeight: function() { return SlidesApp.getActivePresentation().getPageHeight(); }};
+}
+
+// === UTILITY FUNCTIONS ===
 function calculateLuminosity(color) {
   var r = color.asRgbColor().getRed()   / 255;
   var g = color.asRgbColor().getGreen() / 255;
@@ -441,4 +352,9 @@ function calculateLuminosity(color) {
   
   var max = Math.max(r, g, b), min = Math.min(r, g, b);
   return (max + min) / 2;
+}
+
+function roundTo(number, digits) {
+  var power = Math.pow(10, digits);
+  return Math.round(number * power) / power;
 }
